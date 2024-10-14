f7a636883cf4bff89c103ad3532b8a4e65b650f7
Joaquin <hola@jrltt.net>
2024-10-14 22:51:51 +0200

    feat(person page): read data from loader

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,4 +1,9 @@
+import { useLoaderData } from "react-router-dom";
+
 const PersonPage = () => {
+  const { data } = useLoaderData() as { data: { person: any } };
+  console.log(data.person);
+
   return <div>Person</div>;
 };
 
