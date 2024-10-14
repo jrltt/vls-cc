cdc05c36c861d2e3c503a1312fd236c093c85618
Joaquin <hola@jrltt.net>
2024-10-14 23:58:07 +0200

    feat(person page): print data

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,10 +1,36 @@
 import { useLoaderData } from "react-router-dom";
+import { GetPersonByIdQuery } from "../generated/graphql";
 
 const PersonPage = () => {
-  const { data } = useLoaderData() as { data: { person: any } };
-  console.log(data.person);
+  const { data } = useLoaderData() as { data: GetPersonByIdQuery };
+  // FIXME data.person?.species?.averageHeight can be empty
+  // FIXME data.person?.filmConnection?.edges add pagination
+  // and reduce count producers "how many times"
 
-  return <div>Person</div>;
+  return (
+    <>
+      <div>Person {data.person?.name}</div>
+      <div>Birth Year: {data.person?.birthYear}</div>
+      <div>Species: {data.person?.species?.averageHeight}</div>
+      <div>
+        Films:{" "}
+        <ul>
+          {data.person?.filmConnection?.edges?.map((item, idx) => (
+            <li key={idx}>
+              {item?.node?.title}
+              {item?.node?.releaseDate}
+              <ul>
+                Producers:
+                {item?.node?.producers?.map((producer, idx) => (
+                  <li key={idx}>{producer}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default PersonPage;
