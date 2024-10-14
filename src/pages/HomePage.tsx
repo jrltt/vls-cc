0d4823d72db23dbb706706368096c43e0f82466c
Joaquin <hola@jrltt.net>
2024-10-14 22:14:36 +0200

    feat(home page): add loading states and list

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,24 +1,27 @@
-import { gql, useQuery } from "urql";
-
-const query = gql`
-  query GetAllPeople {
-    allPeople {
-      people {
-        id
-        name
-      }
-    }
-  }
-`;
+import { Link } from "react-router-dom";
+import { useQuery } from "urql";
+import { getAllPeopleQuery } from "../queries";
 
 const HomePage = () => {
-  const [data] = useQuery({ query });
-  console.log(data);
+  const [result] = useQuery({ query: getAllPeopleQuery });
+  // console.log(result);
+
+  const { data, fetching, error } = result;
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
 
   return (
     <div>
       Home
-      <pre>{JSON.stringify(data.data?.allPeople?.people, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(data.allPeople?.people, null, 2)}</pre> */}
+      <ul>
+        {data.allPeople?.people.map((person: any) => (
+          <li key={person.id}>
+            <Link to={`/person/${person.id}`}>{person.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
