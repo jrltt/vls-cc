82e86ca94b43c7939d4a6efcd75b05032f7cf273
Joaquin <hola@jrltt.net>
2024-10-16 00:20:12 +0200

    test(use people data hook): add basic use case

---
+++ src/hooks/use-people-data.hook.spec.tsx
@@ -0,0 +1,43 @@
+import { renderHook } from "@testing-library/react";
+import { createClient, Provider } from "urql";
+import { describe, expect, it, vi } from "vitest";
+import { usePeopleData } from "./use-people-data.hook";
+
+vi.mock("@/queries", () => ({
+  getAllPeopleQueryPaginated: "",
+}));
+
+vi.mock("urql", async (importOriginal) => {
+  const mod = await importOriginal();
+  return {
+    ...(mod as any),
+    useQuery: vi.fn(() => [{ data: null, fetching: false, error: null }]),
+  };
+});
+
+const mockClient = createClient({
+  url: "http://localhost:3000",
+});
+describe("Use people data hook", () => {
+  it("should return data on init", () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <Provider value={mockClient}>{children}</Provider>
+    );
+
+    const { result } = renderHook(
+      () =>
+        usePeopleData({
+          people: [{ node: { id: "1", name: "Luke Skywalker" } }],
+          pageInfo: { hasNextPage: false },
+          totalCount: 1,
+        }),
+      { wrapper }
+    );
+
+    // act(() => {
+    //   console.log(result.current.people);
+    // });
+
+    expect(result.current.people).toEqual([]);
+  });
+});
