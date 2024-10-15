c70fb49c185e34176af001af7f7eeb5338c545ed
Joaquin <hola@jrltt.net>
2024-10-15 10:38:37 +0200

    refactor(main): use client

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
+import { graphqlClient } from "@/lib/graphql-client";
 import { loadAllPeople } from "@/loaders/people.loader";
 import { loadPersonById } from "@/loaders/person.loader";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { createClient, Provider } from "urql";
+import { Provider } from "urql";
 import "./index.css";
 import HomePage from "./pages/HomePage";
 import PersonPage from "./pages/PersonPage";
 import { getByPersonId } from "./queries";
 
-const client = createClient({
-  url: "https://swapi-graphql.netlify.app/.netlify/functions/index",
-});
 
 const router = createBrowserRouter([
   {
@@ -28,7 +26,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider value={client}>
+    <Provider value={graphqlClient}>
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
