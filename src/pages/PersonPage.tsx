621da365193c71aa1f2e5efe558c88ed9f378a65
Joaquin <hola@jrltt.net>
2024-10-16 19:10:44 +0200

    feat(person page): film info

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,5 +1,7 @@
 import { Person } from "@/components/person.component";
+import { Planets } from "@/interfaces";
 import { useLoaderData } from "react-router-dom";
+import { Fragment } from "react/jsx-runtime";
 import { GetPersonByIdQuery } from "../generated/graphql";
 
 const PersonPage = () => {
@@ -13,20 +15,31 @@ const PersonPage = () => {
     return <p>Something went wrong</p>;
   }
 
+  function checkPlanetsWithoutWater(planets: Planets) {
+    const total = planets?.filter((item) => item?.surfaceWater === 0);
+
+    return total && total.length > 0 ? total.length : "None";
+  }
   return (
     <>
       <Person {...{ data: person }} />
-
       <div>
-        Films:{" "}
-        <ul>
-          {person?.filmConnection?.edges?.map((item, idx) => (
-            <li key={idx}>
-              {item?.node?.title}
-              {item?.node?.releaseDate}
-            </li>
-          ))}
-        </ul>
+        <h3>Films:</h3>
+        {person?.filmConnection?.edges?.map((item, idx) => (
+          <Fragment key={idx}>
+            Title: {item?.node?.title}
+            <ul className="list-disc list-inside">
+              <li>Release date: {item?.node?.releaseDate}</li>
+              <li>Total planets: {item?.node?.planetConnection?.totalCount}</li>
+              <li>
+                Total planets without water:
+                {checkPlanetsWithoutWater(
+                  item?.node?.planetConnection?.planets
+                )}
+              </li>
+            </ul>
+          </Fragment>
+        ))}
       </div>
     </>
   );
