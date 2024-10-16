d145fa3f2827c69d76503d3782183e6401f8560f
Joaquin <hola@jrltt.net>
2024-10-16 11:12:30 +0200

    feat(person page): add count producers

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,30 +1,71 @@
 import { useLoaderData } from "react-router-dom";
 import { GetPersonByIdQuery } from "../generated/graphql";
 
+type ProducerCount = { producer: string; count: number };
+
+function countProducers(
+  person: NonNullable<GetPersonByIdQuery>["person"]
+): ProducerCount[] {
+  const edges = person?.filmConnection?.edges;
+
+  if (!edges) {
+    return [];
+  }
+
+  return edges.reduce<ProducerCount[]>((acc, filmEdge) => {
+    const producers = filmEdge?.node?.producers ?? [];
+
+    producers.forEach((producer) => {
+      if (!producer) return;
+
+      const existingProducer = acc.find((p) => p.producer === producer);
+
+      if (existingProducer) {
+        existingProducer.count += 1;
+      } else {
+        acc.push({ producer, count: 1 });
+      }
+    });
+
+    return acc;
+  }, []);
+}
+
 const PersonPage = () => {
-  const { data } = useLoaderData() as { data: GetPersonByIdQuery };
+  const {
+    data: { person },
+  } = useLoaderData() as {
+    data: NonNullable<GetPersonByIdQuery>;
+  };
   // FIXME data.person?.species?.averageHeight can be empty
   // FIXME data.person?.filmConnection?.edges add pagination
   // and reduce count producers "how many times"
 
+  const producers = countProducers(person);
+  console.log(producers);
+
   return (
     <>
-      <div>Person {data.person?.name}</div>
-      <div>Birth Year: {data.person?.birthYear}</div>
-      <div>Species: {data.person?.species?.averageHeight}</div>
+      <div>Person {person?.name}</div>
+      <div>Birth Year: {person?.birthYear}</div>
+      <div>Species: {person?.species?.averageHeight}</div>
+      <div>
+        List of producers:
+        <ul>
+          {producers?.map((item, idx) => (
+            <li key={idx}>
+              {item.producer}, count: {item.count}
+            </li>
+          ))}
+        </ul>
+      </div>
       <div>
         Films:{" "}
         <ul>
-          {data.person?.filmConnection?.edges?.map((item, idx) => (
+          {person?.filmConnection?.edges?.map((item, idx) => (
             <li key={idx}>
               {item?.node?.title}
               {item?.node?.releaseDate}
-              <ul>
-                Producers:
-                {item?.node?.producers?.map((producer, idx) => (
-                  <li key={idx}>{producer}</li>
-                ))}
-              </ul>
             </li>
           ))}
         </ul>
