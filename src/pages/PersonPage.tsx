7af0c414b4cc44b5d9cb48bf3ca394ed5ac85ca0
Joaquin <hola@jrltt.net>
2024-10-16 20:58:47 +0200

    feat(person page): add back button

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,7 +1,8 @@
 import { Films } from "@/components/films-component";
 import { Person } from "@/components/person.component";
 import { FilmConnection } from "@/interfaces";
-import { useLoaderData } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
+import { Link, useLoaderData } from "react-router-dom";
 import { GetPersonByIdQuery } from "../generated/graphql";
 
 const PersonPage = () => {
@@ -16,14 +17,21 @@ const PersonPage = () => {
   }
 
   return (
-    <>
+    <div className="space-y-6">
+      <Link
+        className="py-2 px-3 bg-yellow-100 rounded-lg text-xs inline-flex items-center uppercase font-semibold hover:bg-yellow-200 transition-colors hover:border-yellow-300"
+        to="/"
+      >
+        <ArrowLeft className="inline-block mr-2" strokeWidth={3} size={16} />
+        Back to home
+      </Link>
       <Person {...{ data: person }} />
       <Films
         {...{
           personFilms: person?.filmConnection as NonNullable<FilmConnection>,
         }}
       />
-    </>
+    </div>
   );
 };
 
