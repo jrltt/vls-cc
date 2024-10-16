fc1a7ebcfaa55bbea9a940f3a62175944b4dec1a
Joaquin <hola@jrltt.net>
2024-10-16 18:44:55 +0200

    feat(person page): add new component and move logic

---
+++ src/components/person.component.tsx
@@ -0,0 +1,67 @@
+import { GetPersonByIdQuery } from "@/generated/graphql";
+import { PersonCard } from "./person-card.component";
+import { CardContent } from "./ui/card";
+
+type ProducerCount = { producer: string; count: number };
+type Person = NonNullable<GetPersonByIdQuery["person"]>;
+
+function countProducers(person: Person): ProducerCount[] {
+  const edges = person.filmConnection?.edges;
+
+  if (!edges) {
+    return [];
+  }
+
+  return edges.reduce<ProducerCount[]>((acc, filmEdge) => {
+    const producers = filmEdge?.node?.producers ?? [];
+
+    producers.forEach((producer) => {
+      if (!producer) return;
+
+      const existingProducer = acc.find((p) => p.producer === producer);
+
+      if (existingProducer) {
+        existingProducer.count += 1;
+      } else {
+        acc.push({ producer, count: 1 });
+      }
+    });
+
+    return acc;
+  }, []);
+}
+
+export function Person({ data }: { data: Person }) {
+  // FIXME data.person?.filmConnection?.edges add pagination
+  // and reduce count producers "how many times"
+  console.log(data);
+  const producers = countProducers(data);
+  console.log(producers);
+  return (
+    <>
+      <PersonCard
+        id={data.id}
+        gender={data.gender}
+        homeworld={{ name: data.homeworld?.name ?? "" }}
+        name={data.name}
+      >
+        <CardContent className="space-y-4">
+          <div className="flex items-center">Birth year: {data?.birthYear}</div>
+          <div>
+            Species avg. height: {data?.species?.averageHeight ?? "unknown"}
+          </div>
+          <div>
+            List of producers and how many times they worked together:
+            <ul className="list-disc list-inside">
+              {producers?.map((item, idx) => (
+                <li key={idx}>
+                  {item.producer}, times: {item.count}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </CardContent>
+      </PersonCard>
+    </>
+  );
+}
--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,35 +1,6 @@
+import { Person } from "@/components/person.component";
 import { useLoaderData } from "react-router-dom";
 import { GetPersonByIdQuery } from "../generated/graphql";
-
-type ProducerCount = { producer: string; count: number };
-
-function countProducers(
-  person: NonNullable<GetPersonByIdQuery>["person"]
-): ProducerCount[] {
-  const edges = person?.filmConnection?.edges;
-
-  if (!edges) {
-    return [];
-  }
-
-  return edges.reduce<ProducerCount[]>((acc, filmEdge) => {
-    const producers = filmEdge?.node?.producers ?? [];
-
-    producers.forEach((producer) => {
-      if (!producer) return;
-
-      const existingProducer = acc.find((p) => p.producer === producer);
-
-      if (existingProducer) {
-        existingProducer.count += 1;
-      } else {
-        acc.push({ producer, count: 1 });
-      }
-    });
-
-    return acc;
-  }, []);
-}
 
 const PersonPage = () => {
   const {
@@ -37,28 +8,15 @@ const PersonPage = () => {
   } = useLoaderData() as {
     data: NonNullable<GetPersonByIdQuery>;
   };
-  // FIXME data.person?.species?.averageHeight can be empty
-  // FIXME data.person?.filmConnection?.edges add pagination
-  // and reduce count producers "how many times"
 
-  const producers = countProducers(person);
-  console.log(producers);
+  if (!person) {
+    return <p>Something went wrong</p>;
+  }
 
   return (
     <>
-      <div>Person {person?.name}</div>
-      <div>Birth Year: {person?.birthYear}</div>
-      <div>Species: {person?.species?.averageHeight}</div>
-      <div>
-        List of producers:
-        <ul>
-          {producers?.map((item, idx) => (
-            <li key={idx}>
-              {item.producer}, count: {item.count}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Person {...{ data: person }} />
+
       <div>
         Films:{" "}
         <ul>
